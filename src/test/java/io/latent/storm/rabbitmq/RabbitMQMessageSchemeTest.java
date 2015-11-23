8dc9f19f291a03b170f15235c4ab0b738a6d300a
Ivannikov Kirill <kiryam@kiryam.ru>
2015-11-23 13:14:36 +0500

    ArrayList in headers test

--- src/test/java/io/latent/storm/rabbitmq/RabbitMQMessageSchemeTest.java
+++ src/test/java/io/latent/storm/rabbitmq/RabbitMQMessageSchemeTest.java
@@ -3,13 +3,10 @@ package io.latent.storm.rabbitmq;
 import backtype.storm.spout.RawScheme;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Envelope;
-import com.rabbitmq.client.LongString;
 import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.impl.LongStringHelper;
 import org.junit.Test;
-
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -33,8 +30,32 @@ public class RabbitMQMessageSchemeTest {
 
         List<Object> tuple = scheme.deserialize(message);
         RabbitMQMessageScheme.Properties latentProperties = (RabbitMQMessageScheme.Properties) tuple.get(2);
-        latentProperties.getHeaders();
 
         assertEquals("test string long string", latentProperties.getHeaders().get("long_string_header"));
+    }
+
+    @Test
+    public void testDeserializeArrayList() throws Exception{
+        RabbitMQMessageScheme scheme = new RabbitMQMessageScheme(new RawScheme(), "env", "props");
+        Envelope envelope = new Envelope(1, false, "test_exchange", "route1");
+        HashMap<String, Object> headers = new HashMap<String, Object>();
+
+        ArrayList<HashMap<String, Object>> arrayList = new ArrayList<HashMap<String, Object>>();
+
+        HashMap<String, Object> hashMap = new HashMap<String, Object>();
+        hashMap.put("count", 1);
+
+        arrayList.add(hashMap);
+
+        headers.put("x-death", arrayList);
+
+        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().headers(headers).build();
+        QueueingConsumer.Delivery delivery = new QueueingConsumer.Delivery(envelope, properties, new byte[]{});
+        Message message = new Message.DeliveredMessage(delivery);
+
+        List<Object> tuple = scheme.deserialize(message);
+        RabbitMQMessageScheme.Properties latentProperties = (RabbitMQMessageScheme.Properties) tuple.get(2);
+
+        assertEquals(1, ((HashMap) ((ArrayList) latentProperties.getHeaders().get("x-death")).get(0)).get("count"));
     }
 }
